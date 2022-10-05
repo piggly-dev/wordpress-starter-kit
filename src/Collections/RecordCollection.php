6fd290ded952b5020753e7073a5e399d5df3d807
Caique Araujo <caique@studiopiggly.com.br>
2022-10-04 22:47:00 -0300

    :sparkles: feat(collection): Record collection

---
+++ src/Collections/RecordCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Piggly\Wordpress\Collection;
+
+use Exception;
+use Piggly\Wordpress\Post\CustomPostType;
+use Piggly\Wordpress\Post\Interfaces\PostTypeInterface;
+use WP_List_Table;
+
+/**
+ * Record table manipulation.
+ *
+ * @package \Piggly\Wordpress
+ * @subpackage \Piggly\Wordpress\Tables
+ * @version 1.0.7
+ * @since 1.0.7
+ * @category Table
+ * @author Caique Araujo <caique@piggly.com.br>
+ * @author Piggly Lab <dev@piggly.com.br>
+ * @license MIT
+ * @copyright 2022 Piggly Lab <dev@piggly.com.br>
+ */
+abstract class RecordCollection
+{
+}
